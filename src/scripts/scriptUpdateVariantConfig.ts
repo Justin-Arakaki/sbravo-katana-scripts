53312c40b46a48ec3ed58aeca2445fee94b9d60a
Justin-Arakaki <Justiniankoi@gmail.com>
2025-08-07 10:30:51 -0700

    completed product and variant config update script

--- src/scripts/scriptUpdateVariantConfig.ts
+++ src/scripts/scriptUpdateVariantConfig.ts
@@ -3,22 +3,50 @@ import xlsxSheetToJson from '../utils/xlsxSheetToJson';
 import { katanaProduct } from '../api/product';
 import { katanaVariant } from '../api/variant';
 
-const productName = 'B1000 Side Frame';
+type ItemRow = {
+  Description: string;
+  'Engineering Number': string;
+  'Alternate Number': string;
+  Location: string;
+  PDF: string;
+  Notes: string;
+};
+
+// EDIT THESE
+const productName = 'BK Valve Bracket Bar Sub-Assembly, Unfinished';
+const configName = 'Description';
+
+// configName has to match spreadsheet
 
 const productList = await katanaProduct.get({ name: productName });
 const product = productList[0];
 const productId = product.id;
 const variantList = product.variants;
+const variantSkus = variantList.map((variant) => variant.sku);
 
-const partMasterDetails = xlsxSheetToJson('../data/Parts List.xlsx');
-const allDescriptions = partMasterDetails.map((part) => part['Description']);
+const firstVariantConfigs = variantList[0].config_attributes;
+const hasOnlyDescription =
+  firstVariantConfigs.length === 0 ||
+  (firstVariantConfigs.length === 1 &&
+    firstVariantConfigs[0].config_name === 'Description');
 
-console.log(partMasterDetails);
+if (!hasOnlyDescription) {
+  console.log('Avoiding travesty');
+  process.exit();
+}
+
+const partMasterDetails = xlsxSheetToJson(
+  '../data/Parts List.csv'
+) as ItemRow[];
+
+const allDescriptions = partMasterDetails
+  .filter((part) => variantSkus.includes(part['Engineering Number']))
+  .map((part) => part[configName]);
 
 await katanaProduct.update(productId, {
   configs: [
     {
-      name: 'Description',
+      name: configName,
       values: allDescriptions,
     },
   ],
@@ -29,11 +57,15 @@ for (const variant of variantList) {
     (u) => u['Engineering Number'] === variant.sku
   );
 
+  if (typeof part === 'undefined') {
+    continue;
+  }
+
   await katanaVariant.update(variant.id, {
     config_attributes: [
       {
-        config_name: 'Description',
-        config_value: part['Description'],
+        config_name: configName,
+        config_value: part[configName],
       },
     ],
   });
